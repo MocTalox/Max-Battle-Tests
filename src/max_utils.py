d2b135b4f11ef0585c6e6bc292dc69abe07c947f
Moc <m.tagarro@alumnos.upm.es>
2025-10-02 14:54:53 +0200

    Idk how to program in python

--- src/max_utils.py
+++ src/max_utils.py
@@ -6,15 +6,15 @@ from math import floor
 # - Third: total dmg (same as input)
 
 # Calculates the segment damage
-segCalc = lambda dmg, hp : (floor(dmg / hp), dmg % hp, dmg)
+seg_calc = lambda dmg, hp : (floor(dmg / hp), dmg % hp, dmg)
 
-# Converts `segCalc` result to readable string
-segStr = lambda s, xd, td : "Seg: " + str(s) + " | Extra: " + str(xd) + " dmg | Total: " + str(td) + " dmg\n"
+# Converts `seg_calc` result to readable string
+seg_str = lambda s, xd, td : "Seg: " + str(s) + " | Extra: " + str(xd) + " dmg | Total: " + str(td) + " dmg\n"
 
-# Prints `segStr` result to output file
-segPrint = lambda dmg, hp : print(segStr(*segCalc(dmg, hp)))
+# Prints `seg_str` result to output file
+seg_print = lambda dmg, hp : print(seg_str(*seg_calc(dmg, hp)))
 
 # Calculates the possible multiplier values needed to produce
-# the resulting damage, by being multiplied to a base damage:
-# Iow, values which fulfill: floor(base * mult) + 1 = res
+# the resulting damage, by being multiplied to a base damage.
+# IOW, values which fulfill: floor(base * mult) + 1 = res
 mult = lambda res, base : ((res - 1) / base, res / base)
--- src/mult_test.py
+++ src/mult_test.py
@@ -4,62 +4,66 @@ from struct import pack, unpack
 
 # Truncates value to float precision
 # Recursive with lists of values
-def f(x):
+def _to_float(x):
 	if type(x) == int:
 		return x
 	if type(x) == float:
 		return unpack('f', pack('f', x))[0]
-	return list(map(f, x))
+	return list(map(_to_float, x))
 
 # Calculates output damage
-def dmg(atkStats, defStats, bonus, extraBonus):
-	attack = (atkStats[1] + atkStats[2]) * atkStats[3]
-	defense = (defStats[1] + 15) * defStats[2]
-	raw = 0.5 * atkStats[0] * attack / defense * atkStats[4] * atkStats[5] * atkStats[6] * bonus
-	return floor(raw * extraBonus) + 1
+def _dmg(atk_stats, def_stats, bonus, extra_bonus):
+	attack = (atk_stats[1] + atk_stats[2]) * atk_stats[3]
+	defense = (def_stats[1] + 15) * def_stats[2]
+	raw = 0.5 * atk_stats[0] * attack / defense * atk_stats[4] * atk_stats[5] * atk_stats[6] * bonus
+	return floor(raw * extra_bonus) + 1
 
 # Calculates HP and remaining segments sequence
-def hpSeq(totalHp, atks):
-	hp = totalHp
+def _hp_seq(total_hp, atks):
+	hp = total_hp
 	res = [(hp, 100)]
 	for atk in atks:
 		hp = hp - atk
-		res.append((hp, ceil(100 * hp / totalHp)))
+		res.append((hp, ceil(100 * hp / total_hp)))
 	return res
 
 # Product of all bonuses, or itself if its a single value
-def prodBonus(x):
+def _prod_bonus(x):
 	if type(x) == int or type(x) == float:
 		return x
 	return reduce(lambda a, b : a * b, x)
 
-# Calculates an additional bonus value for a test (within the start-end-step range)
-# If segSeq is informed, obtains the range of bonus values that satisfies that sequence
-# Else (segSeq is None), obtains all segment sequence for the range of bonus values
-# - atkStats: List of lists with [power, baseAtk, ivAtk, atkCPM, stab, type, weather]
-# - defStats: List with [totalHp, baseDef, defCPM]
-# - bonus:    Static bonuses, list of friend, helpers, etc
-# - atkSeq:   Attacks sequence
-# - segSeq:   Segment drops sequence (can be None)
-def main(atkStats, defStats, bonus, atkSeq, segSeq, start, end, step, inv = False):
-	resRange = {}
-	atkStats = f(atkStats)
-	defStats = f(defStats)
-	bonus = prodBonus(f(bonus))
+def main(atk_stats, def_stats, bonus, atk_seq, seg_seq, start, end, step, inv = False):
+	"""
+	Calculates an additional bonus value for a test (within the `start`-`end`-`step` range).
+	If `seg_seq` is informed, obtains the range of bonus values that satisfies that sequence.
+	Else (`seg_seq` is `None`), obtains all segment sequence for the range of bonus values.
+	If `inv` is `True`, the bonus will be calculated inverted (useful for defense bonuses).
+	- `atk_stats`: List of lists with [`power`, `base_atk`, `iv_atk`,
+	`atk_cpm`, `stab`, `type`, `weather`], one per each attack type.
+	- `def_stats`: List with [`total_hp`, `base_def`, `def_cpm`].
+	- `bonus`:     Static bonuses, list of `friend`, `helpers`, etc.
+	- `atk_seq`:   Attacks sequence (index of the `atk_stats` list).
+	- `seg_seq`:   Segment drops sequence (can be `None`).
+	"""
+	res_range = {}
+	atk_stats = _to_float(atk_stats)
+	def_stats = _to_float(def_stats)
+	bonus = _prod_bonus(_to_float(bonus))
 	step = round(1 / step)
 	start, end = round(start * step), round(end * step)
 	for k in [x / step for x in range(start, end + 1)]:
-		fk = 1 / f(k) if inv else f(k)
-		dmgs = tuple(map(lambda s : dmg(s, defStats, bonus, fk), atkStats))
-		segs = hpSeq(defStats[0], map(lambda atk : dmgs[atk], atkSeq))
+		fk = 1 / _to_float(k) if inv else _to_float(k)
+		dmgs = tuple(map(lambda s : _dmg(s, def_stats, bonus, fk), atk_stats))
+		segs = _hp_seq(def_stats[0], map(lambda atk : dmgs[atk], atk_seq))
 		segs = list(map(lambda n : segs[n - 1][1] - segs[n][1], range(1, len(segs))))
-		if segSeq == None:
+		if seg_seq == None:
 			print(str(k) + " -> " + str(dmgs) + ": " + str(segs))
-		elif segs == segSeq:
-			resRange[k] = dmgs
-	if len(resRange) != 0:
-		minx, maxx = min(resRange), max(resRange)	
-		print(str(minx) + " -> " + str(resRange[minx]))
-		print(str(maxx) + " -> " + str(resRange[maxx]))
-	elif segSeq != None:
+		elif segs == seg_seq:
+			res_range[k] = dmgs
+	if len(res_range) != 0:
+		minx, maxx = min(res_range), max(res_range)	
+		print(str(minx) + " -> " + str(res_range[minx]))
+		print(str(maxx) + " -> " + str(res_range[maxx]))
+	elif seg_seq != None:
 		print("No result found on range: " + str(start / step) + " - " + str(end / step))
